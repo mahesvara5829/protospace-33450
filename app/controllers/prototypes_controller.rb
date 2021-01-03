f506067af2c5e8e7935e2ec9328c655f4ef37478
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-03 15:08:40 +0900

    プロトタイプ保存後パス修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -14,7 +14,7 @@ class PrototypesController < ApplicationController
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
-      redirect_to prototype_path
+      redirect_to root_path
     else
       render :new
     end
